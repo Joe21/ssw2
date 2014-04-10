87bc70ff83f215c5358306539afca580b445fd73
Joe21 <joej21@gmail.com>
2014-04-10 01:31:21 -0400

    almost ready for deployment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,23 @@ class UsersController < ApplicationController
 	before_filter :authenticate_user!
 
 	def add_counter
+		current_user.increment(:counter) 
+		current_user.save
+		redirect_to root_path
 	end
 
 	def minus_counter
+		if current_user.counter > 0
+			current_user.decrement(:counter) 
+			current_user.save
+		end
+		redirect_to root_path
+	end
+
+	def reset_counter
+		current_user.update_attribute :counter, 0
+		current_user.save
+		redirect_to root_path
 	end
 
 # Establish strong params for user model
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Ssw2::Application.routes.draw do
   root to: 'entries#index'
   resources :entries
 
-  post '/users/:id/add_counter' => 'users#add_counter', as: 'add'
-  post '/users/:id/minus_counter' => 'users#minus_counter', as: 'minus'
+  post '/users/add_counter' => 'users#add_counter', as: 'add'
+  post '/users/minus_counter' => 'users#minus_counter', as: 'minus'
+  post '/users/reset_counter' => 'users#reset_counter', as: 'reset'
 end
+
+        # <form action='<%= add_path %>' method='post'>
+          # <button type="submit">+</button>
+        # </form>
